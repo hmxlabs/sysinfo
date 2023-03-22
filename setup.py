9484f33b54a5c44b3f707f907ff34a8e6cebb06a
hamza-x <hamza-x@users.noreply.github.com>
2023-03-22 19:03:26 +0000

    Improved readme a little more

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_namespace_packages
 setup(
     
     name = "hmxlabs.sysinfo",
-    version = "1.0.1",
+    version = "1.0.3",
     py_modules= ['hmxlabs.sysinfo.sysinfo'],
     packages = find_namespace_packages('src')
 )
