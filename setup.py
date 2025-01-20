c95ac644fe1114cfd72b89f418845d8207abb8fe
Hamza M <hamza-x@users.noreply.github.com>
2025-01-20 12:28:27 +0000

    increment version to create new minor release to PyPi

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_namespace_packages
 setup(
     
     name = "hmxlabs.sysinfo",
-    version = "1.0.8",
+    version = "1.0.9",
     py_modules= ['hmxlabs.sysinfo.sysinfo'],
     packages = find_namespace_packages('src')
 )
