215f10b6d689ef82103e684339ac8987eaf29049
Hamza M <hamza-x@users.noreply.github.com>
2025-08-21 07:43:21 +0200

    Fix build process. Update script to create dedicated env, remove setup.py and update pyproject.toml to include those details

--- build.sh
+++ build.sh
@@ -1,1 +1,29 @@
+#!/bin/bash
+
+if [ -d "dist" ]; then
+    echo "Removing old dist directory"
+    rm -rf dist
+fi
+
+# Check if a buildenv exists  
+if [ -d "buildenv" ]; then
+    echo "Buildenv already exists"
+    source buildenv/bin/activate
+else
+    echo "Creating buildenv"
+    python3 -m venv buildenv
+    source buildenv/bin/activate
+    pip3 install build
+fi
+
+# Create a buildenv
+python3 -m venv buildenv
+
+# Build the Python package
+echo "Building Python package"
 python3 -m build
+
+if [ $? -ne 0 ]; then
+    echo "Python package build failed. Exiting."
+    exit 1
+fi
